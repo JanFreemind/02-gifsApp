11504ff70e0c8a3353a45ede6b8f96aabab764a5
Jan <juan.perezlosa@gmail.com>
2022-02-20 13:32:35 +0100

    Fin sección 7

--- src/app/gifs/services/gifs.service.ts
+++ src/app/gifs/services/gifs.service.ts
@@ -1,23 +1,27 @@
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpParams } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { SearchGifsResponse, Gif } from '../interface/gifs.interface';
 
 @Injectable({
   providedIn: 'root'
 })
 export class GifsService {
 
-  private apiKey: string = 'v1xDI9I1D9qtWoCOXLm3cvBvkxYwIvwo';
-
-  private _historial: string[] = [];
+  private apiKey     : string = 'v1xDI9I1D9qtWoCOXLm3cvBvkxYwIvwo';
+  private servicioUrl: string = 'https://api.giphy.com/v1/gifs';
+  private _historial : string[] = [];
 
   // TODO: Cambiar any por su tipo 
-  public resultados: any[] = [];
+  public resultados: Gif[] = [];
 
   get historial() {
     return [...this._historial];
   }
 
   constructor( private http: HttpClient) {
+
+    this._historial = JSON.parse(localStorage.getItem('historial')!) || [];
+    this.resultados = JSON.parse(localStorage.getItem('resultados')!) || [];
 
   }
 
@@ -28,12 +32,20 @@ export class GifsService {
     if( ! this._historial.includes( query ) ) {
       this._historial.unshift( query );
       this._historial = this._historial.splice(0,10);
+
+      localStorage.setItem ('historial', JSON.stringify( this._historial ) );
+
     }
 
-    this.http.get(`https://api.giphy.com/v1/gifs/search?api_key=v1xDI9I1D9qtWoCOXLm3cvBvkxYwIvwo&q=${ query }&limit=10`)
-      .subscribe( ( resp: any ) => {
-        console.log( resp.data );
+    const params = new HttpParams()
+          .set('api_key', this.apiKey)
+          .set('limit', '10')
+          .set('q', query );   
+
+    this.http.get<SearchGifsResponse>(`${ this.servicioUrl }/search`, { params } )
+      .subscribe( ( resp ) => {
         this.resultados = resp.data;
+        localStorage.setItem ('resultados', JSON.stringify(this.resultados) );
       });
 
   }
